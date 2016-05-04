89b541ed8a15f943ac7da9e79eaafdd46876fe23
PhilippPro <philipp_probst@gmx.de>
2016-05-04 14:38:29 +0200

    added some measures

--- MulticlassAUC.R
+++ MulticlassAUC.R
@@ -16,21 +16,41 @@ model = train(lrn, iris.task)
 pred = predict(model, iris.task)
 
 library(caTools)
-m = colAUC(getPredictionProbabilities(pred)[,1], pred$data$truth)
-# First column: j setosa, 
+m = colAUC(getPredictionProbabilities(pred), pred$data$truth)
+# First column: j setosa
 colAUC(getPredictionProbabilities(pred)[,1], pred$data$truth)
 
-# Is this Hand and Till?
-mean(m[col(m)!=4-row(m)])
-colMeans(m)
+# Comparison of colAUC with pROC
+library(pROC)
+# Syntax (response, predictor):
+result_pROC = auc(droplevels(pred$data$truth[pred$data$truth %in% c("versicolor", "virginica")]), getPredictionProbabilities(pred)[,2][pred$data$truth %in% c("versicolor", "virginica")])
+result_pROC == m[3,2]
+result_pROC = auc(droplevels(pred$data$truth[pred$data$truth %in% c("setosa", "virginica")]), getPredictionProbabilities(pred)[,3][pred$data$truth %in% c("setosa", "virginica")])
+result_pROC == m[2,3]
+# Seems to be reliable!
 
-m = colAUC(getPredictionProbabilities(pred), pred$data$truth)
-c = t(combn(1:nlevels(pred$data$truth), 2, simplify = T))
-m[cbind(1:nrow(c),c[,2])]
-mean(m[cbind(1:nrow(c),c[,2])])
+multiclass.auc3(getPredictionProbabilities(pred), pred$data$truth)
 
+
+truth = pred$data$truth
+probabilities = getPredictionProbabilities(pred)
 # AUNU
-for(j in 1:nlevels(pred$data$truth)){
-  
-}
+mean(vnapply(1:nlevels(truth), function(i) colAUC(getPredictionProbabilities(pred)[,i], truth == levels(truth)[i])))
 
+# AUNP
+sum(vnapply(1:nlevels(truth), function(i) mean(truth == levels(truth)[i]) * colAUC(getPredictionProbabilities(pred)[,i], truth == levels(truth)[i])))
+
+# AU1U (Hand and Till)
+m = colAUC(getPredictionProbabilities(pred), truth)
+mean(m[col(m) != ncol(m) + 1 - row(m)])
+# or
+c = t(combn(1:nlevels(truth), 2, simplify = T))
+mean(m[cbind(1:nrow(c), c[, 2])])
+
+# AU1P
+m = colAUC(getPredictionProbabilities(pred), truth)
+weights = table(truth[-1]) / length(truth[-1])
+m = m * matrix(rep(weights, each = length(weights)), ncol = length(weights))
+sum(m[col(m) != ncol(m) + 1 - row(m)]) / (ncol(m) - 1)
+
+# ensure that it works, also when no observation is present!!
