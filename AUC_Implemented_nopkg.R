a3151396a44f4c3284e919f5d7ab44c6f76f9430
RaphaelCouronne <raphael.couronne@gmail.com>
2016-05-06 12:02:05 +0200

    Added all AUC and examples
    Same results for :
    
    AUC / AUNU / AUNP / AU1U / AU1P
    
    Scored AUC and Probabilistic AUC have no comparison yet

--- AUC_Implemented_nopkg.R
+++ AUC_Implemented_nopkg.R
@@ -46,7 +46,8 @@ binaryclass.auc = function(pred, truth,  names=FALSE) {
     matrixI.tempj=makeMatrixI(j,j,pred)
     for (k in c(1:K)) {
       if (k!=j) {
-        AUC[j,k]=t(f[,j])%*%matrixI.tempj%*%f[,k]/(f.summed[j]*f.summed[k])
+        value=t(f[,j])%*%matrixI.tempj%*%f[,k]/(f.summed[j]*f.summed[k])
+        AUC[j,k]=max(value,1-value)
       }
     }
   }
@@ -59,6 +60,45 @@ binaryclass.auc = function(pred, truth,  names=FALSE) {
   return(AUC)
 }
 
+
+## AUNU ------
+multiclass.aunu= function(pred, truth,  names=FALSE) {
+  mean(vnapply(1:nlevels(truth), function(i) binaryclass.auc(cbind(pred[,i],1-pred[,i]), as.factor(truth == levels(truth)[i]))[1,2]))
+}
+
+## AUNP ------
+multiclass.aunp= function(pred, truth,  names=FALSE) {
+  # Matrix f
+  f= model.matrix( ~ 0 + truth, truth)
+  n=dim(f)[1]
+  p=dim(f)[2]
+  f.proba=apply(f,2,sum)/n
+  
+  # Computation
+  dim(pred)[2]*mean(vnapply(1:nlevels(truth), function(i) f.proba[i]*binaryclass.auc(cbind(pred[,i],1-pred[,i]), as.factor(truth == levels(truth)[i]))[1,2]))
+}
+
+
+
+## AU1U ------
+multiclass.au1u= function(pred, truth,  names=FALSE) {
+  AUC.matrix<-binaryclass.auc(pred, truth,  names=FALSE)
+  mean(AUC.matrix[row(AUC.matrix)!=col(AUC.matrix)])
+}
+
+## AU1P ------
+multiclass.au1p= function(pred, truth,  names=FALSE) {
+  # Matrix f
+  f= model.matrix( ~ 0 + truth, truth)
+  n=dim(f)[1]
+  p=dim(f)[2]
+  f.proba=apply(f,2,sum)/n
+  
+  # Computation
+  AUC.matrix<-binaryclass.auc(pred, truth,  names=FALSE)
+  AUC.matrix.weightsproba<-t(t(AUC.matrix)*f.proba)
+  return(mean(AUC.matrix.weightsproba[row(AUC.matrix.weightsproba)!=col(AUC.matrix.weightsproba)])*p)
+}
 
 
 ## Scored AUC ------
@@ -191,8 +231,17 @@ truth=iris$Species
 
 ## Computation of AUC measures
 
+# Classic AUC
 binaryclass.auc(predicted, truth, names = TRUE)
+multiclass.aunu(predicted, truth, names = TRUE)
+multiclass.aunp(predicted, truth, names = TRUE)
+multiclass.au1u(predicted, truth, names = TRUE)
+multiclass.au1p(predicted, truth, names = TRUE)
+
+#Scored AUC
 binaryclass.Scoredauc(predicted, truth, names = TRUE)
 multiclass.Scoredauc(predicted, truth, names = TRUE)
+
+# Probabilistic AUC
 binaryclass.Probabilisticauc(predicted, truth, names = TRUE)
 multiclass.Probabilisticauc(predicted, truth, names = TRUE)
