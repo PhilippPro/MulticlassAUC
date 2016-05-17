8ee0a121aa7059c7fa05916127f1bd79ad506fce
PhilippPro <philipp_probst@gmx.de>
2016-05-17 15:33:29 +0200

    implemented the multiclass AUC functions in mlr

--- AUCmlr.R
+++ AUCmlr.R
@@ -1,45 +1,157 @@
-
-#' @export multiclass.auc
-#' @rdname measures
-#' @format none
-multiclass.auc = makeMeasure(id = "multiclass.auc", minimize = FALSE, best = 1, worst = 0,
-                             properties = c("classif", "classif.multi", "req.pred", "req.truth", "req.prob"),
-                             name = "Multiclass area under the curve",
-                             note = "Calls `pROC::multiclass.roc`.",
-                             fun = function(task, model, pred, feats, extra.args) {
-                               requirePackages("pROC", why = "multiclass.auc", default.method = "load")
-                               resp = pred$data$response
-                               predP = getPredictionProbabilities(pred)
-                               # choose the probablity of the choosen response
-                               predV = vnapply(seq_row(predP), function(i) {
-                                 predP[i, resp[i]]
-                               })
-                               auc = pROC::multiclass.roc(response = resp, predictor = predV)$auc
-                               as.numeric(auc)
-                             }
-)
-
-###############################################################################
-### classif binary ###
-###############################################################################
-#' @export auc
-#' @rdname measures
-#' @format none
-auc = makeMeasure(id = "auc", minimize = FALSE, best = 1, worst = 0,
-                  properties = c("classif", "req.pred", "req.truth", "req.prob"),
-                  name = "Area under the curve",
-                  fun = function(task, model, pred, feats, extra.args) {
-                    # ROCR does not work with NAs
-                    if (anyMissing(pred$data$response) || length(unique(pred$data$truth)) == 1L)
-                      return(NA_real_)
-                    measureAUC(getPredictionProbabilities(pred), pred$data$truth, pred$task.desc$negative, pred$task.desc$positive)
-                  }
-)
-
-#' @export measureAUC
-#' @rdname measures
-#' @format none
-measureAUC = function(probabilites, truth, negative, positive) {
-  rpreds = asROCRPredictionIntern(probabilites, truth, negative, positive)
-  ROCR::performance(rpreds, "auc")@y.values[[1L]]
+# File: R/measures_colAUC.R
+# mainly copied from the caTools package
+colAUC = function(probabilities, truth) {
+  y = as.factor(truth)
+  X = as.matrix(probabilities)
+  if (nrow(X) == 1)
+    X = t(X)
+  nR = nrow(X)
+  nC = ncol(X)
+  nY = table(y)
+  uL = as.factor(rownames(nY))
+  nL = length(nY)
+  if (nL <= 1) 
+    stop("colAUC: List of labels 'y' have to contain at least 2 class labels.")
+  if (!is.numeric(X)) 
+    stop("colAUC: 'X' must be numeric")
+  if (nR != length(y)) 
+    stop("colAUC: length(y) and nrow(X) must be the same")
+  L = matrix(rep(uL, each = nR), nR, nL)
+  per = combs(1:nL, 2)
+  nP = nrow(per)
+  Auc = matrix(0.5, nP, nC)
+  rownames(Auc) = paste(uL[per[, 1]], " vs. ", uL[per[, 2]], sep = "")
+  colnames(Auc) = colnames(X)
+  # Wilcoxon AUC
+  idxL = vector(mode = "list", length = nL)
+  for (i in 1:nL) idxL[[i]] = which(y == uL[i])
+  for (j in 1:nC) {
+    for (i in 1:nP) {
+      c1 = per[i, 1]
+      c2 = per[i, 2]
+      n1 = as.numeric(nY[c1])
+      n2 = as.numeric(nY[c2])
+      if (n1 > 0 & n2 > 0) {
+        r = rank(c(X[idxL[[c1]], j], X[idxL[[c2]], j]))
+        Auc[i, j] = (sum(r[1:n1]) - n1 * (n1 + 1)/2)/(n1 * n2)
+      }
+    }
+  }
+  Auc = pmax(Auc, 1 - Auc)
+  return(Auc)
 }
+
+combs = function(v,k) {
+  # combs(V,K) - finds all unordered combinations of K elements from vector V 
+  #  V is a vector of length N
+  #  K is a integer 
+  # combs(V,K) creates a matrix with N!/((N-K)! K!) rows
+  # and K columns containing all possible combinations of N elements taken K at a time.
+  # example: combs(1:3,2) returns matrix with following rows (1 2), (1 3), (2 3)
+  n = length(v)
+  if      (n==k  ) P = matrix(v,1,n)
+  else if (k==1  ) P = matrix(v,n,1)
+  else if (k==n-1) P = matrix( rep(v, each=n-1), n, n-1)
+  else if (k< n) {
+    P = matrix(0,0,k)
+    if (k < n & k > 1) {
+      for (i in 1:(n-k+1)) {
+        Q = combs(v[(i+1):n],k-1)
+        j = nrow(Q)
+        P = rbind(P, cbind(rep(v[i],j), Q))
+      }
+    }
+  } else 
+    stop("combs: number m has to be smaller or equal to length of vector v")
+  return(P)
+}
+
+# File: R/measures.R (delete old multiclass.auc!)
+#' @export multiclass.aunu
+#' @rdname measures
+#' @format none
+multiclass.aunu = makeMeasure(id = "multiclass.aunu", minimize = FALSE, best = 0, worst = 1,
+                               properties = c("classif", "classif.multi", "req.pred", "req.truth", "req.prob"),
+                               name = "Average multiclass AUC",
+                               note = "Following the definition in the Ferri et. al Paper: https://www.math.ucdavis.edu/~saito/data/roc/ferri-class-perf-metrics.pdf",                             
+                               fun = function(task, model, pred, feats, extra.args) {
+                                   measureAUNU(getPredictionProbabilities(pred), pred$data$truth)
+                               }
+)
+
+#' @export measureAUNU
+#' @rdname measures
+#' @format none
+measureAUNU = function(probabilities, truth) {
+  if (nlevels(truth) == 2L) probabilities = cbind(probabilities,1-probabilities)
+  mean(vnapply(1:nlevels(truth), function(i) colAUC(probabilities[, i], truth == levels(truth)[i])))
+}
+
+#' @export multiclass.aunp
+#' @rdname measures
+#' @format none
+multiclass.aunp = makeMeasure(id = "multiclass.aunp", minimize = FALSE, best = 0, worst = 1,
+                              properties = c("classif", "classif.multi", "req.pred", "req.truth", "req.prob"),
+                              name = "Weighted average multiclass AUC",
+                              note = "Following the definition in the Ferri et. al paper: https://www.math.ucdavis.edu/~saito/data/roc/ferri-class-perf-metrics.pdf",                             
+                              fun = function(task, model, pred, feats, extra.args) {
+                                measureAUNP(getPredictionProbabilities(pred), pred$data$truth)
+                              }
+)
+
+#' @export measureAUNP
+#' @rdname measures
+#' @format none
+measureAUNP = function(probabilities, truth) {
+  if (nlevels(truth) == 2L) probabilities = cbind(probabilities,1-probabilities)
+  sum(vnapply(1:nlevels(truth), function(i) mean(truth == levels(truth)[i]) * colAUC(probabilities[,i], truth == levels(truth)[i])))  
+}
+
+#' @export multiclass.au1u
+#' @rdname measures
+#' @format none
+multiclass.au1u = makeMeasure(id = "multiclass.au1u", minimize = FALSE, best = 0, worst = 1,
+                              properties = c("classif", "classif.multi", "req.pred", "req.truth", "req.prob"),
+                              name = "Average multiclass AUC with each class against each other",
+                              note = "Following the definition in the Ferri et. al paper: https://www.math.ucdavis.edu/~saito/data/roc/ferri-class-perf-metrics.pdf",                             
+                              fun = function(task, model, pred, feats, extra.args) {
+                                measureAU1U(getPredictionProbabilities(pred), pred$data$truth)
+                              }
+)
+
+#' @export measureAU1U
+#' @rdname measures
+#' @format none
+measureAU1U = function(probabilities, truth) {
+  if (nlevels(truth) == 2L) probabilities = cbind(probabilities,1-probabilities)
+  m = colAUC(probabilities, truth)
+  c = c(combn(1:nlevels(truth), 2))
+  mean(m[cbind(rep(1:nrow(m), each = 2), c)])
+}
+
+#' @export multiclass.au1p
+#' @rdname measures
+#' @format none
+multiclass.au1p = makeMeasure(id = "multiclass.au1p", minimize = FALSE, best = 0, worst = 1,
+                              properties = c("classif", "classif.multi", "req.pred", "req.truth", "req.prob"),
+                              name = "Weighted average multiclass AUC with each class against each other",
+                              note = "Following the definition in the Ferri et. al paper: https://www.math.ucdavis.edu/~saito/data/roc/ferri-class-perf-metrics.pdf",                             
+                              fun = function(task, model, pred, feats, extra.args) {
+                                measureAU1P(getPredictionProbabilities(pred), pred$data$truth)
+                              }
+)
+
+#' @export measureAU1P
+#' @rdname measures
+#' @format none
+measureAU1P = function(probabilities, truth) {
+  if (nlevels(truth) == 2L) probabilities = cbind(probabilities,1-probabilities)
+  m = colAUC(probabilities, truth)
+  weights = table(truth) / length(truth)
+  m = m * matrix(rep(weights, each = nrow(m)), ncol = length(weights))
+  c = c(combn(1:nlevels(truth), 2))
+  sum(m[cbind(rep(1:nrow(m), each = 2), c)]) / (nlevels(truth) - 1)
+}
+
+# File: tests/testthat/test_base_measures.R (delete old multiclass.auc)
+# not sure about how the tests should look like!
--- MulticlassAUC.R
+++ MulticlassAUC.R
@@ -1,27 +1,28 @@
 library(mlr)
 
 lrn = makeLearner("classif.lda", predict.type = "prob")
-model = train(lrn, iris.task)
-pred = predict(model, iris.task)
+model = train(lrn, sonar.task)
+pred = predict(model, sonar.task)
 
-truth = pred$data$truth
+performance(pred, measures = list(multiclass.aunu, multiclass.aunp, multiclass.au1u, multiclass.au1p))
+
+# Multiclass Example
+library(OpenML)
+data = getOMLTask(task.id = 3548, verbosity=0)$input$data.set
+
+multiclass.task = makeClassifTask(id = "abc", data = data$data, target = "Type")
+model = train(lrn, multiclass.task)
+pred = predict(model, multiclass.task)
+performance(pred, measures = list(multiclass.aunu, multiclass.aunp, multiclass.au1u, multiclass.au1p, logloss, multiclass.brier))
+
 probabilities = getPredictionProbabilities(pred)
 
 library(caTools)
 m = colAUC(probabilities, pred$data$truth)
-# First column: j setosa
+# First column: j 
 colAUC(probabilities[,1], pred$data$truth)
 
-# Comparison of colAUC with pROC
-library(pROC)
-# Syntax (response, predictor):
-result_pROC = auc(droplevels(pred$data$truth[pred$data$truth %in% c("versicolor", "virginica")]), probabilities[,2][pred$data$truth %in% c("versicolor", "virginica")])
-result_pROC == m[3,2]
-result_pROC = auc(droplevels(pred$data$truth[pred$data$truth %in% c("setosa", "virginica")]), probabilities[,3][pred$data$truth %in% c("setosa", "virginica")])
-result_pROC == m[2,3]
-# Seems to be reliable!
-
-# multiclass.auc3(getPredictionProbabilities(pred), pred$data$truth)
+truth = pred$data$truth
 
 # AUNU
 mean(vnapply(1:nlevels(truth), function(i) colAUC(probabilities[,i], truth == levels(truth)[i])))
@@ -36,28 +37,9 @@ mean(m[cbind(rep(1:nrow(m), each = 2), c)])
 
 # AU1P
 m = colAUC(probabilities, truth)
+c = c(combn(1:nlevels(truth), 2))
 weights = table(truth) / length(truth)
 m = m * matrix(rep(weights, each = nrow(m)), ncol = length(weights))
 sum(m[cbind(rep(1:nrow(m), each = 2), c)]) / (nlevels(truth) - 1)
 
 # ensure that it works, also when no observation is present and in higher class case!
-
-# k = 4
-source("/nfsmb/koll/probst/Scoring_rules/MulticlassAUC/AUC_Implemented_nopkg.R")
-library(pROC)
-
-data(aSAH)
-probabilities=(cbind(aSAH$s100b,sample(aSAH$s100b-0.3),sample(aSAH$s100b+2),sample(aSAH$s100b)))
-truth = factor(aSAH$gos6, ordered = FALSE)
-
-m = colAUC(probabilities, truth)
-c = c(combn(1:nlevels(truth), 2))
-mean(m[cbind(rep(1:nrow(m), each = 2), c)])
-
-binaryclass.auc(probabilities, truth)
-multiclass.au1u(probabilities, truth)
-# seems good!
-
-binaryclass.auc.fast(probabilities, truth) # transposed version of the slow one
-multiclass.au1u.fast(probabilities, truth)
-# also good
